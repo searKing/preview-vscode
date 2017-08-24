e8ce369721cd64b06be5675013b677b30661cc31
searKing <searKingChan@gmail.com>
2017-08-25 02:32:22 +0800

    [BUG]fix cmd missing on vscode1.15.1

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,46 @@
-"use strict";
-import {
-    workspace, window, ExtensionContext, commands,
-    TextEditor, TextDocumentContentProvider, EventEmitter,
-    Event, Uri, TextDocumentChangeEvent, ViewColumn,
-    TextEditorSelectionChangeEvent,
-    TextDocument, Disposable
-} from "vscode";
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
 import { PreviewDocumentContentProvider } from "./previewDocumentContentProvider";
 
-// 主函数
-export function activate(context: ExtensionContext) {
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
 
-    console.log("Preview Extension Startup");
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, Preview Extension Startup');
+
+
     // 文本内容提供者
     let provider: PreviewDocumentContentProvider;
-    let registeration: Disposable;
+    let registeration: vscode.Disposable;
 
     // 向vscode注册当前文件发生变化时的回调函数
-    workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
-        if (!!e && !!window.activeTextEditor && e.document === window.activeTextEditor.document) {
+    vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
+        if (!!e && !!vscode.window.activeTextEditor && e.document === vscode.window.activeTextEditor.document) {
             // 由于文档变动必然在插件启动之后，而插件启动时就已经创建了provider
             // 因此不存在该变量未定义的问题
             provider.update();
         }
     });
 
-    window.onDidChangeTextEditorSelection((e: TextEditorSelectionChangeEvent) => {
-        if (!!e && !!e.textEditor && (e.textEditor === window.activeTextEditor)) {
+    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+        if (!!e && !!e.textEditor && (e.textEditor === vscode.window.activeTextEditor)) {
             provider.update();
         }
     })
+
 
     function registerPreviewDocumentContentProvider() {
         provider = new PreviewDocumentContentProvider();
         // 向vscode为文本内容数据库注册一个URI的协议scheme，以后均可通过该协议与文本内容数据库进行交互
         // html-preview 通过这个scheme访问的内容，都是通过该provider获得的
-        registeration = workspace.registerTextDocumentContentProvider(PreviewDocumentContentProvider.previewScheme, provider);
+        registeration = vscode.workspace.registerTextDocumentContentProvider(PreviewDocumentContentProvider.previewScheme, provider);
     }
     // 调用vscode系统命令预览当前HTML页面
-    function sendPreviewCommand(displayColumn: ViewColumn): void {
+    function sendPreviewCommand(displayColumn: vscode.ViewColumn): void {
         registerPreviewDocumentContentProvider();
         // 给vscode发送预览该临时HTML文件的命令
         provider.sendPreviewCommand(displayColumn).catch(function (error) {
@@ -49,58 +51,69 @@ export function activate(context: ExtensionContext) {
     // 调用vscode系统命令返回当前之前的页面
     function sendBackviewCommand(): PromiseLike<void> {
         // 给vscode发送返回预览之前页面的位置
-        return commands.executeCommand("workbench.action.navigateBack").then((success) => {
+        return vscode.commands.executeCommand("workbench.action.navigateBack").then((success) => {
         }, (reason) => {
             console.warn(reason);
-            window.showErrorMessage(reason);
+            vscode.window.showErrorMessage(reason);
         });
     }
     // 调用vscode系统命令关闭当前预览的页面
     function sendCloseviewCommand(): PromiseLike<void> {
         // 给vscode发送返回预览之前页面的位置
-        return commands.executeCommand("workbench.action.closeActiveEditor").then((success) => {
+        return vscode.commands.executeCommand("workbench.action.closeActiveEditor").then((success) => {
         }, (reason) => {
             console.warn(reason);
-            window.showErrorMessage(reason);
+            vscode.window.showErrorMessage(reason);
         });
     }
-    function getSpiltColumn(): ViewColumn {
-        let displayColumn: ViewColumn;
+    function getSpiltColumn(): vscode.ViewColumn {
+        let displayColumn: vscode.ViewColumn;
         // 在拆分窗口右侧显示预览界面，若当前待预览HTML文件在最右侧，则覆盖显示
-        switch (window.activeTextEditor.viewColumn) {
-            case ViewColumn.One:
-                displayColumn = ViewColumn.Two;
+        switch (vscode.window.activeTextEditor.viewColumn) {
+            case vscode.ViewColumn.One:
+                displayColumn = vscode.ViewColumn.Two;
                 break;
-            case ViewColumn.Two:
-            case ViewColumn.Three:
-                displayColumn = ViewColumn.Three;
+            case vscode.ViewColumn.Two:
+            case vscode.ViewColumn.Three:
+                displayColumn = vscode.ViewColumn.Three;
                 break;
             default:
-                displayColumn = window.activeTextEditor.viewColumn;
+                displayColumn = vscode.window.activeTextEditor.viewColumn;
                 break;
         }
         return displayColumn;
     }
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
     // 命令回调函数，该命令在package.json中与快捷方式、菜单选项等关联
+    // 覆盖显示预览界面
+    let previewDisposable = vscode.commands.registerCommand('extension.preview', () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        // vscode.window.showInformationMessage('Hello preview!');
+        if (!vscode.window.activeTextEditor) {
+            return sendBackviewCommand();
+        }
+        return sendPreviewCommand(vscode.window.activeTextEditor.viewColumn);
+    });
+
     // 侧边栏打开预览界面
-    let previewToSide = commands.registerCommand("extension.previewToSide", () => {
-        if (!window.activeTextEditor) {
+    let previewToSideDisposable = vscode.commands.registerCommand('extension.previewToSide', () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        // vscode.window.showInformationMessage('Hello previewToSide!');
+        if (!vscode.window.activeTextEditor) {
             return sendCloseviewCommand();
         }
-        let displayColumn: ViewColumn = getSpiltColumn();
+        let displayColumn: vscode.ViewColumn = getSpiltColumn();
         return sendPreviewCommand(displayColumn);
     });
 
-    // 覆盖显示预览界面
-    let preview = commands.registerCommand("extension.preview", () => {
-        if (!window.activeTextEditor) {
-            return sendBackviewCommand();
-        }
-        return sendPreviewCommand(window.activeTextEditor.viewColumn);
-    });
-
-    // 注册当前插件由激活变为非激活状态后，自动销毁这些回调函数和资源
-    context.subscriptions.push(preview, previewToSide, registeration);
+    context.subscriptions.push(previewDisposable, previewToSideDisposable);
 }
 
 // this method is called when your extension is deactivated
