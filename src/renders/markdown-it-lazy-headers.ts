8ac068da62312a4b4b2e430ffa2e4fe436778a11
searKing <471030698@qq.com>
2024-03-17 18:29:13 +0800

    feat: add config.markdown.lazy-headers.enabled.

--- src/renders/markdown-it-lazy-headers.ts
+++ src/renders/markdown-it-lazy-headers.ts
@@ -8,11 +8,6 @@ export namespace MarkdownItLazyHeaders {
     // This method is called when your extension is activated
     // Your extension is activated the very first time the command is executed
     export function extendMarkdownIt(context: vscode.ExtensionContext | undefined, md: MarkdownIt): MarkdownIt {
-        function isEnabled(): boolean {
-            const config = vscode.workspace.getConfiguration('markdown');
-            return config.get<boolean>('lazy-headers.enabled', true);
-        }
-
         if (!!context) {
             vscode.workspace.onDidChangeConfiguration(e => {
                 if (e.affectsConfiguration(markdownLazyHeadersSetting)) {
@@ -20,7 +15,9 @@ export namespace MarkdownItLazyHeaders {
                 }
             }, undefined, context.subscriptions);
         }
-        if (!isEnabled()) {
+
+        const config = vscode.workspace.getConfiguration('markdown');
+        if (!config.get<boolean>('lazy-headers.enabled', true)) {
             return md;
         }
         return md.use(require('markdown-it-lazy-headers'), {});
