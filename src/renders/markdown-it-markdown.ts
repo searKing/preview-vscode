b3bff97974b1064a57044a38a12da4d8e109957c
searKing <471030698@qq.com>
2024-12-13 11:18:49 +0800

    build: use esModuleInterop for extensions

--- src/renders/markdown-it-markdown.ts
+++ src/renders/markdown-it-markdown.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import MarkdownIt = require('markdown-it');
+import type MarkdownIt = require('markdown-it');
 import { dedent } from 'ts-dedent';
 import { githubEngine } from './markdown-it-engine';
 
--- src/renders/markdown-it.ts
+++ src/renders/markdown-it.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import MarkdownIt = require('markdown-it');
+import type MarkdownIt = require('markdown-it');
 import { githubEngine } from './markdown-it-engine';
 
 export type ExtendMarkdownIt<T = any> = (context: vscode.ExtensionContext | undefined, md: MarkdownIt, options?: T) => MarkdownIt;
